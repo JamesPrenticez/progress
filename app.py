d0a29c176f5a76284d0312d129d4b24aafd8edb8
James Prentice <jamesprenticez@gmail.com>
2022-08-10 21:47:08 +1200

    Threading

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
+import random
+import threading
+import time
 
-from flask import Flask, app, render_template
+from flask import Flask, render_template
 
+
+class ExportingThread(threading.Thread):
+    def __init__(self):
+        self.progress = 0
+        super().__init__()
+
+    def run(self):
+        # Your exporting stuff goes here ...
+        for _ in range(100000):
+            time.sleep(1)
+            self.progress += 10
+
+
+exporting_threads = {}
 app = Flask(__name__)
-app.jinja_env.cache = {}
+app.debug = True
 
-@app.route("/")
+
+@app.route('/')
 def index():
-  return render_template("index.html", message="nothing")
+    global exporting_threads
 
-if __name__ == "__main__":
-  app.run()
+    thread_id = random.randint(0, 10000)
+    exporting_threads[thread_id] = ExportingThread()
+    exporting_threads[thread_id].start()
+
+    return render_template('index.html', thread_id=thread_id)
+
+
+@app.route('/progress/<int:thread_id>')
+def progress(thread_id):
+    global exporting_threads
+
+    return str(exporting_threads[thread_id].progress)
+
+
+if __name__ == '__main__':
+    app.run()
